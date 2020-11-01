5f7294c3a5abf1a034a2f025b0a4b31b95007e40
Radu Domnu <rdomnu@redhat.com>
2020-11-01 15:45:16 +0100

    Few updates, demo working

---
+++ scripts/vault-policy-and-role.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# export NS 
+export NS=rhtr2020-rhsso-vault-crunchy
+# export VAULT_* variables
+
+## Configure Static Secrets
+
+vault secrets enable -path=sso kv-v2
+
+## Create policies for accessing those paths
+
+cat <<EOF > policy.hcl
+path "sso/*" {
+  capabilities = ["read", "list"]
+}
+path "database/creds/*" {
+  capabilities = ["read", "list"]
+}
+EOF
+
+vault policy write sso-policy policy.hcl
+
+## Create Role 
+vault write auth/kubernetes/role/sso \
+     bound_service_account_names='*' \
+     bound_service_account_namespaces=$NS \
+     policies=sso-policy \
+     ttl=0s
